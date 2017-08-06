2c26877523a8a46bc8990ec827bb776724d59d88
Jonas Deitmerg <jonasdeitmerg@aim.com>
2017-08-06 19:25:43 +0200

    Add external interface to CPU
    Interface includes ROM input, ROM io and clock input.

--- cpu.vhdl
+++ cpu.vhdl
@@ -3,12 +3,22 @@ use ieee.std_logic_1164.all;
 use work.common.all;
 
 entity cpu is
-
+    port ( clk : std_logic; -- Global clock
+           -- Shall write at rising edge of global clock if ram_we = '1'.
+           -- Shall read asynchronously.
+           ram_read : in word_t;
+           ram_addr, ram_write : out word_t;
+           ram_we : out std_logic;
+           -- Instructions are expected to come in on the rising edge of
+           -- rom_clk:
+           rom_clk : out std_logic;
+           rom_read : in word_t;
+           rom_addr : out word_t
+         );
 end cpu;
 
 architecture arch of cpu is
     -- Plenty of signals between the CPU's components:
-    signal clk : std_logic; -- global clock signal
 
     -- Data written to register or memory
     signal data_bus : word_t;
@@ -18,7 +28,6 @@ architecture arch of cpu is
     signal dsrc_selector : dsrc_t;
 
     signal instr_immediate : word_t;
-    signal mem_addr, mem_read, mem_write : word_t;
 
     -- carry, zero and negative flag for flow control
     signal flags_read, flags_write : flags_t;
@@ -34,8 +43,6 @@ architecture arch of cpu is
     signal arg0, arg1, alu_res : word_t;
     signal alu_op : alu_op_t;
 
-    -- For instruction decoder:
-    signal instruction : word_t;
 begin
     flow_controller : entity work.flow_cntrl
         port map ( flags => flags_read,
@@ -69,9 +76,12 @@ begin
                    flags_out => flags_write
                  );
 
+    -- Instructions are loaded half a cycle before they take effect:
+    rom_clk <= not clk;
+    rom_addr <= pc_read;
     instruction_dec : entity work.instr_decoder
         port map ( clk => clk,
-                   instruction => instruction,
+                   instruction => rom_read,
                    alu_op => alu_op,
                    flow_control => fc_command,
                    data_src => dsrc_selector,
@@ -79,15 +89,20 @@ begin
                    reg_dest => reg_w_sel,
                    reg_src0 => reg_r_sel0,
                    reg_src1 => reg_r_sel1,
-                   flags_we => flags_we
+                   flags_we => flags_we,
+                   mem_we => ram_we
                  );
 
     -- Data bus demultiplexer:
     data_sources(dsrc_ALU) <= alu_res;
     data_sources(dsrc_arg0) <= arg0;
     data_sources(dsrc_immediate) <= instr_immediate;
-    data_sources(dsrc_mem) <= mem_read;
+    data_sources(dsrc_mem) <= ram_read;
     data_bus <= data_sources(dsrc_selector);
+
+    -- The second ALU argument is used as address by memory access
+    -- instructions.
+    ram_addr <= arg1;
 
 end arch;
 
