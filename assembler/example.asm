92b24248fef2c41cc808bea568ce8242fcab9ac6
Jonas Deitmerg <jonasdeitmerg@aim.com>
2017-08-09 00:36:14 +0200

    Add flow control to example assembly

--- assembler/example.asm
+++ assembler/example.asm
@@ -3,4 +3,12 @@ ldi r1, -4
 add r2, r1, r0
 ldi r3, 0
 stm r3, r2 ; Save result of addition at address 0x00
+
+tst r0
+sns ; skip following two instructions if negative flag
+; is set
+ldi r3, 4
+mv r0, r0
+stm r3, r2
+
 mv pc, pc ; loop forever
